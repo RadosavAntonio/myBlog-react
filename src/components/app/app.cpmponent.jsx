e33d37908d1b1e5b48aa83e399508a01be39ddf2
Antonio Radosav <radosav.antonio@gmail.com>
2021-03-15 15:20:41 +0000

    restart webpage

---
+++ src/components/app/app.cpmponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Header } from '../header/header.component'
+
+export const App = () => {
+  return (
+    <div>
+      <Header />
+      test
+    </div>
+  )
+}
